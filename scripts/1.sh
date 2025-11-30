673cc1442d63b75c5dc647ac60fa8281cb352ccd
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2025-12-01 07:56:51 +0900

    update images

---
+++ scripts/1.sh
@@ -0,0 +1,45 @@
+python test.py ../sample_images/0001.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+python test.py ../sample_images/0002.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+python test.py ../sample_images/0003.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+python test.py ../sample_images/0004.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+python test.py ../sample_images/0005.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+python test.py ../sample_images/0006.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+python test.py ../sample_images/0007.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+python test.py ../sample_images/0008.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+python test.py ../sample_images/0009.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+python test.py ../sample_images/0010.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+python test.py ../sample_images/0011.png \
+  --user-prompt ./user_prompt.txt \
+  --system-prompt ./system_prompt3.txt
+
+
